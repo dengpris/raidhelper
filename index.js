a44ecd4797c2b86ca04b08911ab3a66b2ec24c86
Priscilla <priscilla.deng@mail.utoronto.ca>
2020-10-29 02:21:33 -0400

    Organized code with module exports

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const ms = require('ms');
 const xlsxFile = require('read-excel-file/node');
 
 // Login with Token
-const token = fs.readFileSync('C:/Users/prisc/Documents/token.txt').toString();
+const token = fs.readFileSync('C:/Users/Priscilla Deng/Documents/token.txt').toString();
 const PREFIX = '!';
+
+bot.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js')) // Only read JS files
+for (const file of commandFiles){
+	const command = require(`./commands/${file}`);
+
+	bot.commands.set(command.name, command);
+}
 
 bot.on('ready', () => {
 	console.log('Bot is online.');
@@ -15,103 +23,35 @@ bot.on('message', message=>{
 	// 'let' declares variables locally, 'var' decalres globally
 	// Here we are splitting string by white space
 	let args = message.content.substring(PREFIX.length).split(" ");
+	
 	let mainrole = message.guild.roles.cache.find(role => role.name === "new role");
 	let muterole = message.guild.roles.cache.find(role => role.name === "mute");
 	let time;
 
 	switch(args[0]){
 		case 'ping':
-			message.channel.send('pong!')
+			bot.commands.get('ping').execute(message, args);
 			break;
 		case 'website':
-			message.channel.send('https://www.youtube.com/channel/UCBkKEr-U-VWYbw27oZcIO_A/featured')
+			bot.commands.get('website').execute(message, args);
 			break;
-			// Use: !clear + #, # determines how many messages to delete
-			// Note specifying a number simply deletes the most recent message
 		case 'clear':
-			if (args[1]) message.channel.bulkDelete(args[1])
-			else message.channel.bulkDelete(2)
+			bot.commands.get('clear').execute(message, args);
 			break;
 		case 'crab':
-			message.reply('I will mute you')
-			let author = message.member
-			if (!muterole) return message.reply("Mute role not found");
-			time = '5s';
-			
-			author.roles.remove(mainrole.id);
-			author.roles.add(muterole.id);
-
-			setTimeout(function(){
-				author.roles.add(mainrole.id);
-				author.roles.remove(muterole.id);
-				message.channel.send(`${author} has been unmuted!`)
-			}, ms(time));
+			bot.commands.get('crab').execute(message, args);
 			break;
 		case 'mute':
-			let person = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]))
-			if (!person) return message.reply("User not found");
-			if (!muterole) return message.reply("Mute role not found");
-
-			time = args[2];
-			if (!time) return message.reply("you didn't specify a time!");
-
-			person.roles.remove(mainrole.id);
-			person.roles.add(muterole);
-			message.channel.send(`${person} has been muted for ${ms(ms(time))}`);
-
-			setTimeout(function(){
-				person.roles.add(mainrole.id);
-				person.roles.remove(muterole.id);
-				message.channel.send(`${person} has been unmuted!`)
-			}, ms(time));
+			bot.commands.get('mute').execute(message, args);
 			break;
 		case 'help':
-			message.channel.send("```BigBrainsBot supports the following commands: \n\n- !ping \n- !website \n- !clear # \n- !serebii den # \n- !serebii convert # ```")
+			bot.commands.get('help').execute(message, args);
 			break;
 		case 'user':
-			if (args[1]){
-				let member = message.mentions.users.first()
-				if (!member) return message.reply("member could not be found.");
-
-				const user = new Discord.MessageEmbed()
-				.setTitle('User Information')
-				.setThumbnail(member.displayAvatarURL())
-				.addField('Player Name', member.username)
-				.addField('Tag', member.tag)
-				.setColor(0x48C9B0);
-				message.channel.send(user);
-
-			} else {
-				const user = new Discord.MessageEmbed() // Create embed
-				.setTitle('User Information')
-				.setThumbnail(message.author.displayAvatarURL())
-				.addField('Player Name', message.author.username)
-				.addField('Tag', message.author.tag)
-				.setColor(0x48C9B0);
-				message.channel.send(user);
-		}
-
+			bot.commands.get('user').execute(message, args);
 			break;
 		case 'serebii':
-			// Return den info on Serebii
-			if (args[1] === 'den'){
-				if (args[2] > 0 && args[2] <= 100){
-					message.channel.send('https://www.serebii.net/swordshield/maxraidbattles/den' + args[2] + '.shtml')
-				} else {
-					message.channel.send('Den out of range.')
-				}
-			}
-			// Convert den # to Pkhex den #
-			else if (args[1] === 'convert'){
-				if (args[2] > 0 && args[2] <= 100){
-					// Common or rare den
-
-					convert(args[2]);
-				}
-			}
-			else {
-				message.channel.send('Command not recognized. Perhaps you mean to try `!serebii convert #` or `!serebii den #` ?')
-			}
+			bot.commands.get('serebii').execute(message, args);
 			break;
 	}
 
